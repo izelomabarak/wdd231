b5b893141faa9c12634c1130a88cf5443a4114cd
izelomabarak <joseluisim.lds@gmail.com>
2025-10-10 23:06:07 -0600

    f

--- proyect/scripts/date.js
+++ proyect/scripts/date.js
@@ -3,11 +3,4 @@ const timestamp = document.querySelector("#timestamp");
 
 const today = new Date();
 
-year.innerHTML = ` © <span class="highlight">${today.getFullYear()}</span> Puebla Trade Federation`;
-
-const modified = document.querySelector("#modified");
-
-let lastModified = new Date(document.submition);
-const formattedDate = `${(lastModified.getMonth() + 1).toString().padStart(2, '0')}/${lastModified.getDate().toString().padStart(2, '0')}/${lastModified.getFullYear()} ${lastModified.getHours().toString().padStart(2, '0')}:${lastModified.getMinutes().toString().padStart(2, '0')}:${lastModified.getSeconds().toString().padStart(2, '0')}`;
-
-modified.innerHTML = `Last Modification: <span class="highlight">${formattedDate}</span>`;
+year.innerHTML = ` © <span class="highlight">${today.getFullYear()}</span> Greenhouses Gidance Group`;
